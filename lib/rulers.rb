10de0ab94d1cb6b08fbfbddb0d1f0cb83c36b2df
Anonyo Hossain <anonyo.hossain@gmail.com>
2015-11-09 21:48:40 -0500

    Add sum method to Array

--- lib/rulers.rb
+++ lib/rulers.rb
@@ -1,10 +1,11 @@
 require "rulers/version"
+require "rulers/array"
 
 module Rulers
   class Application
     def call(env)
       [200, {'Content-Type' => 'text/html'},
-       ["Hello from Ruby on Rulers!"]]
+       ["<h1>Hello from Ruby on Rulers</h1>"]]
     end
   end
 end
